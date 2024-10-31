0713b1a2ea867ff33693ff5b77bfbd72be7bfb4d
Clester31 <christianlester114@gmail.com>
2024-10-31 17:42:45 -0400

    updated documentation

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Mind Map Prototype
+
+## TODO
+
+* Have some sort of way to connect the nodes to each other with a line
+* Clean up the look of the nodes
+* maybe a popup to make adding a node a little more fluid
+* replace image search prompt with something other than a prompt() (another popup maybe?)
--- src/app.js
+++ src/app.js
@@ -30,6 +30,7 @@ async function searchForImage(query) {
 // get the position of where the user is clicking currenty.
 // This is used to determing where the node is placed
 function getClickPosition(event) {
+    // my best attempt at getting the node to appear dead center of the click
     return {
         x: event.clientX - 100,
         y: event.clientY - 50,
@@ -46,37 +47,38 @@ function createNode(x, y, nodeType) {
     node.className = 'node rounded-full p-4 absolute flex flex-col items-center text-center';
     node.style.left = `${x}px`;
     node.style.top = `${y}px`;
-    node.innerText = nodeType;
+    node.innerText = nodeType; // set the text to one of the three node types
 
     nodeInput.className = 'border-2 border-black rounded-lg text-center'
 
     removeNodeBtn.className = 'bg-red-500 px-2 text-white';
     removeNodeBtn.innerText = "remove";
-    removeNodeBtn.addEventListener('click', () => canvas.removeChild(node));
+    removeNodeBtn.addEventListener('click', () => canvas.removeChild(node)); // remove the node when the button is clicked
 
-    setNodeBorder(node, nodeType);
+    setNodeBorder(node, nodeType); // set color of the node based on type
     node.appendChild(nodeInput);
     node.appendChild(removeNodeBtn);
 
+    // if the node is an 'asset node' the user can add an image to that node
     if (nodeType === 'asset') {
         const imgBtn = document.createElement('button');
         imgBtn.innerText = 'add image';
+        // method for handling image adding
         imgBtn.addEventListener('click', async () => {
             const query = prompt('Enter your search query:');
             if (query) {
                 const images = await searchForImage(query);
                 if (images.length > 0) {
-                    const popup = createImagePopup(images, node); 
+                    const popup = createImagePopup(images, node); // show the image selection popup
                     canvas.appendChild(popup);
-                    nodeInput.style.display = 'none'; 
+                    nodeInput.style.display = 'none'; // hide the input field
                 } else {
-                    alert('No images found.');
+                    alert('No images found.'); // image search returns no results
                 }
             }
         });
         node.appendChild(imgBtn);
     }
-
     return node;
 }
 
@@ -100,7 +102,8 @@ function createImagePopup(images, node) {
     popup.innerHTML = `
     <div class="bg-gray-300 p-2 rounded-xl w-1/2 flex flex-col justify-center m-auto items-center" style="z-index: 9999; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);">
         <div class="grid grid-cols-5 gap-4">
-            ${images.map((image, index) => {
+            ${images.map((image, index) => { // go through each image using map and add it to the list of selectable images
+                // each image is a buiton that once clicked, adds the image to the node
                 return `<button class='add-img' data-url="${image.previewURL}"><img src="${image.previewURL}" alt="Image ${index + 1}" class="mb-2"></button>`;
             }).join('')}
         </div>
@@ -109,10 +112,13 @@ function createImagePopup(images, node) {
         </button>
     </div>
     `;
+    // handle closing the image popup if the 'Close' button is clicked
     popup.querySelector('.close-popup').addEventListener('click', () => {
         canvas.removeChild(popup);
         nodeInput.style.display = 'block';
     });
+    // for each image, if the image is clicked then we get that image's info
+    // and pass it over to the function responsible for handling adding it to the node
     popup.querySelectorAll('.add-img').forEach(imgBtn => {
         imgBtn.addEventListener('click', () => {
             const imageURL = imgBtn.getAttribute('data-url');
