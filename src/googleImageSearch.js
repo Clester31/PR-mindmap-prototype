8ff7a8a0bdd8bbccf00fc3bcce277d423cad1690
Christian Lester <91839534+Clester31@users.noreply.github.com>
2024-11-05 21:15:57 -0500

    Delete src directory

--- src/googleImageSearch.js
+++
@@ -1,9 +0,0 @@
-// const apiKey = 'AIzaSyDAa_DWnSUwZCEQ3GojHlrJhBKAAT89dbw'
-// const engineId = '103f947ca17b6456e'
-
-// const GoogleImages = require('google-images');
-// const client = new GoogleImages(engineId, apiKey);
-
-// export function searchForImage() {
-//     client.search('Steve Angello', {page: 2});
-// }
